8ccba7aae320db527247f1e52f2de1baa7c8e3ba
Maciej Sypien <maciejsypien@gmail.com>
2014-06-13 03:52:32 +0200

    Fixed filtering for video extensions; Added program total time execution

--- select_movie.py
+++ select_movie.py
@@ -4,12 +4,75 @@
 import os
 import sys
 import random
+import time
+
 
 
 # Global variables
 __version__ = "1.0.0"
 __programName__ = os.path.basename(__file__)
 __author__ = "Maciej Sypień"
+
+video_extensions = [
+    '.264', '.3g2', '.3gp', '.3gp2', '.3gpp', '.3gpp2',
+    '.3mm', '.3p2', '.60d', '.787', '.890', '.aaf', '.aec', '.aep',
+    '.aepx', '.aet', '.aetx', '.ajp', '.ale', '.am', '.amc', '.amv',
+    '.amx', '.anim', '.aqt', '.arcut', '.arf', '.asf', '.asx', '.avb',
+    '.avc', '.avchd', '.avd', '.avi', '.avp', '.avs', '.avs', '.avv',
+    '.awlive', '.axm', '.bdm', '.bdmv', '.bdt2', '.bdt3', '.bik',
+    '.bin', '.bix', '.bmc', '.bmk', '.bnp', '.box', '.bs4', '.bsf',
+    '.bu', '.bvr', '.byu', '.camproj', '.camrec', '.camv', '.ced',
+    '.cel', '.cine', '.cip', '.clk', '.clpi', '.cmmp', '.cmmtpl',
+    '.cmproj', '.cmrec', '.cpi', '.cst', '.cvc', '.cx3', '.d2v',
+    '.d3v', '.dash', '.dat', '.dav', '.db2', '.dce', '.dck', '.dcr',
+    '.dcr', '.ddat', '.dif', '.dir', '.divx', '.dlx', '.dmb',
+    '.dmsd', '.dmsd3d', '.dmsm', '.dmsm3d', '.dmss', '.dmx', '.dnc',
+    '.dpa', '.dpg', '.dream', '.dsy', '.dv', '.dv-avi', '.dv4',
+    '.dvdmedia', '.dvr', '.dvr-ms', '.dvx', '.dxr', '.dzm', '.dzp',
+    '.dzt', '.edl', '.evo', '.eye', '.ezt', '.f4f', '.f4p', '.f4v',
+    '.fbr', '.fbr', '.fbz', '.fcp', '.fcproject', '.ffd', '.flc',
+    '.flh', '.fli', '.flv', '.flx', '.fpdx', '.ftc', '.gcs', '.gfp',
+    '.gl', '.gom', '.grasp', '.gts', '.gvi', '.gvp', '.h264',
+    '.hdmov', '.hdv', '.hkm', '.ifo', '.imovieproj',
+    '.imovieproject', '.inp', '.int', '.ircp', '.irf', '.ism',
+    '.ismc', '.ismclip', '.ismv', '.iva', '.ivf', '.ivr', '.ivs',
+    '.izz', '.izzy', '.jmv', '.jss', '.jts', '.jtv', '.k3g',
+    '.kdenlive', '.kmv', '.ktn', '.lrec', '.lrv', '.lsf', '.lsx',
+    '.lvix', '.m15', '.m1pg', '.m1v', '.m21', '.m21', '.m2a', '.m2p',
+    '.m2t', '.m2ts', '.m2v', '.m4e', '.m4u', '.m4v', '.m75', '.mani',
+    '.meta', '.mgv', '.mj2', '.mjp', '.mjpg', '.mk3d', '.mkv',
+    '.mmv', '.mnv', '.mob', '.mod', '.modd', '.moff', '.moi',
+    '.moov', '.mov', '.movie', '.mp21', '.mp21', '.mp2v', '.mp4',
+    '.mp4.infovid', '.mp4v', '.mpe', '.mpeg', '.mpeg1', '.mpeg4',
+    '.mpf', '.mpg', '.mpg2', '.mpgindex', '.mpl', '.mpl', '.mpls',
+    '.mproj', '.mpsub', '.mpv', '.mpv2', '.mqv', '.msdvd', '.mse',
+    '.msh', '.mswmm', '.mts', '.mtv', '.mvb', '.mvc', '.mvd', '.mve',
+    '.mvex', '.mvp', '.mvp', '.mvy', '.mxf', '.mxv', '.mys', '.ncor',
+    '.nsv', '.nut', '.nuv', '.nvc', '.ogm', '.ogv', '.ogx', '.orv',
+    '.osp', '.otrkey', '.pac', '.par', '.pds', '.pgi', '.photoshow',
+    '.piv', '.pjs', '.playlist', '.plproj', '.pmf', '.pmv', '.pns',
+    '.ppj', '.prel', '.pro', '.pro4dvd', '.pro5dvd', '.proqc',
+    '.prproj', '.prtl', '.psb', '.psh', '.pssd', '.pva', '.pvr',
+    '.pxv', '.qt', '.qtch', '.qtindex', '.qtl', '.qtm', '.qtz',
+    '.r3d', '.rcd', '.rcproject', '.rdb', '.rec', '.rm', '.rmd',
+    '.rmd', '.rmp', '.rms', '.rmv', '.rmvb', '.roq', '.rp', '.rsx',
+    '.rts', '.rts', '.rum', '.rv', '.rvid', '.rvl', '.sbk', '.sbt',
+    '.scc', '.scm', '.scm', '.scn', '.screenflow', '.sdv', '.sec',
+    '.sedprj', '.seq', '.sfd', '.sfvidcap', '.siv', '.smi', '.smi',
+    '.smil', '.smk', '.sml', '.smv', '.snagproj', '.spl', '.sqz',
+    '.srt', '.ssf', '.ssm', '.stl', '.str', '.stx', '.svi', '.swf',
+    '.swi', '.swt', '.tda3mt', '.tdt', '.tdx', '.thp', '.tid',
+    '.tivo', '.tix', '.tod', '.tp', '.tp0', '.tpd', '.tpr', '.trec',
+    '.trp', '.ts', '.tsp', '.ttxt', '.tvlayer', '.tvrecording',
+    '.tvs', '.tvshow', '.usf', '.usm', '.vbc', '.vc1', '.vcpf',
+    '.vcr', '.vcv', '.vdo', '.vdr', '.vdx', '.veg', '.vem', '.vep',
+    '.vf', '.vft', '.vfw', '.vfz', '.vgz', '.vid', '.video',
+    '.viewlet', '.viv', '.vivo', '.vix', '.vlab', '.vob', '.vp3',
+    '.vp6', '.vp7', '.vpj', '.vro', '.vs4', '.vse', '.vsp', '.w32',
+    '.wcp', '.webm', '.wlmp', '.wm', '.wmd', '.wmmp', '.wmv', '.wmx',
+    '.wot', '.wp3', '.wpl', '.wtv', '.wve', '.wvx', '.xej', '.xel',
+    '.xesc', '.xfl', '.xlmv', '.xmv', '.xvid', '.y4m', '.yog',
+    '.yuv', '.zeg', '.zm1', '.zm2', '.zm3', '.zmv']
 
 
 class bcolors:
@@ -32,8 +95,6 @@ def main():
                 directories.append(path)
                 #print_all_files(get_filelist_from_dirs(filename))
 
-            print(directories)
-
             s = (
                 "Known video extensions: " +
                 bcolors.HEADER + "{0}" + bcolors.ENDC + "\n"
@@ -41,9 +102,9 @@ def main():
                 bcolors.OKBLUE + "{1}" + bcolors.ENDC + "\n"
                 "Found video files: " +
                 bcolors.OKGREEN + "{2}" + bcolors.ENDC + "\n"
-                "Movie for tonight is: " +
+                "Movie for today is: " +
                 bcolors.WARNING + "{3}" + bcolors.ENDC
-                ).format(len(get_list_of_video_extensions()),
+                ).format(len(video_extensions),
                          directories[0],
                          len(get_filelist_from_dirs(directories[0])),
                          get_random_movie(directories[0]))
@@ -87,14 +148,9 @@ def get_filelist_from_dirs(directory):
     for root, dirs, files in os.walk(directory):
         # print path to all filenames.
         for file in files:
-            list_of_files.append(os.path.join(root, file))
-
-        # Advanced usage:
-        # editing the 'dirnames' list will stop os.walk() from
-        # recursing into there.
-        if '.git' in root:
-            # don't go into any .git directories.
-            dirs.remove('.git')
+            fileName, fileExtension = os.path.splitext(file)
+            if fileExtension in video_extensions:
+                list_of_files.append(os.path.join(root, file))
     return list_of_files
 
 
@@ -102,15 +158,35 @@ def get_random_item(my_list):
     return random.choice(my_list)
 
 
-def print_all_files(my_list):
+def print_all_files(my_list, show_line_numbers=True):
+    """Printing all elements in my_list.
+
+    Keywords:
+    my_list -- List of items
+    show_line_numbers -- (Default: True) adding at the start of line
+                         a current index number.
+    """
     for index, file in enumerate(my_list):
-        print(index, ") ", file, sep='')
+        if(show_line_numbers):
+            print(index, ") ", file, sep='')
+        else:
+            print(file)
 
 
-def get_random_movie(list_of_dirs):
-    list_of_files = get_filelist_from_dirs(list_of_dirs)
-    movie = get_random_item(list_of_files).split('/')
-    return movie[-1]
+def get_random_movie(my_list):
+    """Retrun single random element from a list.
+
+    Keywords:
+    my_list -- List of items
+    """
+    try:
+        if not my_list:
+            raise Exception("List is empty")
+        list_of_files = get_filelist_from_dirs(my_list)
+        movie = get_random_item(list_of_files).split('/')
+        return movie[-1]
+    except Exception as err:
+        print(err)
 
 
 def get_list_of_video_extensions():
@@ -124,4 +200,6 @@ def get_list_of_video_extensions():
 
 
 # Run the program
+start_time = time.time()
 main()
+print("Total time exec:",time.time() - start_time, "seconds")
